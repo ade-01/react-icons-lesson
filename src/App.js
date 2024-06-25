6f03406cce99dd9343822934ca0fc9768d9f5213
ade-01 <oladelezainb01@gmail.com>
2024-06-25 20:57:56 +0100

    updated

--- src/App.js
+++ src/App.js
@@ -1,32 +1,43 @@
-/* import './App.css'; */
+import './App.css';
 import { Routes, Route, Link } from 'react-router-dom';
 import IconComponent from "./components/icon-component/IconComponent";
 import About from './components/pages/about/About';
 import Contact from './components/pages/contact/Contact';
 import Home from './components/pages/home/Home';
 
+
 function App() {
   return (
     <div className="App">
-     <nav>
-      <ul>
-        <li>
-          <Link to='/'>Home</Link>
-        </li>
-        <li>
-          <Link to='/about'>About</Link>
-        </li>
-        <li>
-          <Link to='/contact'>Contact</Link>
-        </li>
-      </ul>
+     <nav className='navbar navbar-expand-lg navbar-light bg-secondary'>
+      <div className='container-fluid'>
+        <Link className='navbar-brand' to='/'> 
+        <img src="/logo.jpg" alt=""  width={100} height={100} className='rounded-circle'/>
+        </Link>
+      </div>
+      <div className='collapse navbar-collapse ' id='navBarNav'>
+        <ul className='navbar-nav'>
+          <li className='nav-item'>
+            <Link className='nav-link' to='/'>Home</Link>
+          </li>
+          <li className='nav-item'>
+            <Link className='nav-link' to='/about'>About</Link>
+          </li>
+          <li className='nav-item'>
+            <Link className='nav-link' to='/contact'>Contact</Link>
+          </li>
+        </ul>
+      </div>
      </nav>
 
-     <Routes>
-      <Route path='/' element={ <Home/> }/>
-      <Route path='/about' element={ <About/> }/>
-      <Route path='/contact' element={ <Contact/> }/>
-     </Routes>
+    <div className='container mt-3'>
+      <Routes>
+            <Route path='/' element={ <Home/> }/>
+            <Route path='/about' element={ <About/> }/>
+            <Route path='/contact' element={ <Contact/> }/>
+      </Routes>
+    </div>
+    
     {/*  <IconComponent/> */}
     
     </div>
